4d952600b6ef150a72621e611da0514cc88434cf
k4io404 <116021310+k4io404@users.noreply.github.com>
2025-11-08 09:53:26 -0300

    Teste.java

--- src/main/java/com/controleDeAcesso/Teste.java
+++ src/main/java/com/controleDeAcesso/Teste.java
@@ -1,10 +1,27 @@
 package com.controleDeAcesso;
 
+import com.controleDeAcesso.dao.*;
+import com.controleDeAcesso.service.*;
+import com.controleDeAcesso.dto.*;
+import com.controleDeAcesso.model.*;
+
+import java.sql.SQLException;
+import java.util.Date;
+
 public class Teste {
 
     public static void main(String[] args) {
 
+        MoradorDTO mDTO = new MoradorDTO();
+        mDTO.setNome("Ana");
+        mDTO.setCpf("99999999998");
+        System.out.println("incluindo1");
+        mDTO.setDataNasc(new Date());
+        mDTO.setAtiva(true);
+        mDTO.setTipo("M");
+        System.out.println("incluindo2");
 
-
+        MoradorService m = new MoradorService();
+        m.incluirMorador(mDTO);
     }
 }
